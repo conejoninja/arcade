07f25fc15baf810e432bec583f14b4bb70528d69
Daniel Esteban <conejo@theninjabunny.com>
2025-09-27 01:02:03 +0200

    snake for gopher ARCADE!

--- main.go
+++ main.go
@@ -7,6 +7,13 @@ import (
 	"github.com/conejoninja/arcade/tinyssd1306"
 )
 
+const (
+	UP = iota
+	RIGHT
+	DOWN
+	LEFT
+)
+
 var (
 	sda_pin machine.Pin = machine.P2
 	scl_pin machine.Pin = machine.P3
@@ -17,11 +24,13 @@ var (
 
 	display *tinyssd1306.Device
 
-	player    uint8 = 60
-	oldPlayer uint8 = 61
-
-	shootsX [3]uint8 = [3]uint8{0, 0, 0}
-	shootsY [3]uint8 = [3]uint8{0, 0, 0}
+	snake                   [128]uint8
+	snakeLength             uint8
+	snakeDirection          uint8
+	snakeHead               uint8
+	apple                   uint8
+	i                       uint8
+	bounceLeft, bounceRight bool
 )
 
 func main() {
@@ -30,80 +39,129 @@ func main() {
 	btnRight.Configure(machine.PinConfig{Mode: machine.PinInputPullup})
 	speaker.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
-	for {
-		beep(50, 250)
-		time.Sleep(100 * time.Millisecond)
-		beep(150, 150)
-		time.Sleep(100 * time.Millisecond)
-		beep(250, 50)
-		time.Sleep(100 * time.Millisecond)
-	}
-
 	display = tinyssd1306.New(machine.P2, machine.P3)
 	display.Configure()
 	display.ClearScreen()
 
-	shootsY[0] = 55
-	shootsX[0] = player
-
-	display.SetPos(61, 0)
-	display.DataStart()
-	display.DataByte(0xFF)
-	display.DataByte(0xF0)
-	display.DataByte(0x78)
-	display.DataByte(0x3C)
-	display.DataByte(0x1E)
-	display.DataByte(0x0F)
-	display.DataStop()
+	startGame()
 
 	for {
-		if btnLeft.Get() && player > 0 {
-			player -= 3
+		if btnLeft.Get() {
+			if !bounceLeft {
+				bounceLeft = true
+				if snakeDirection == 0 {
+					snakeDirection = 3
+				} else {
+					snakeDirection--
+				}
+			}
+		} else {
+			bounceLeft = false
 		}
-		if btnRight.Get() && player < 120 {
-			player += 3
+		if btnRight.Get() {
+			if !bounceRight {
+				bounceRight = true
+				if snakeDirection == 3 {
+					snakeDirection = 0
+				} else {
+					snakeDirection++
+				}
+			}
+		} else {
+			bounceRight = false
 		}
 
-		if shootsY[0] == 0 {
-			display.SetPos(shootsX[0], 0)
-			display.DataStart()
-			display.DataByte(0x00)
-			display.DataStop()
-
-			shootsY[0] = 55
-			shootsX[0] = player
-		}
-		shootsY[0]--
-		DrawShoot(0)
-
-		if oldPlayer != player {
-
-			ClearPlayer(oldPlayer)
-			DrawPlayer(player)
-			oldPlayer = player
-		}
-
-		//time.Sleep(50 * time.Millisecond)
-
+		moveSnake()
 	}
 }
 
-func DrawPlayer(x uint8) {
-	display.SetPos(x, 63)
+func moveSnake() {
+	snakeHead = snake[0]
+	switch snakeDirection {
+	case RIGHT:
+		if snakeHead%16 == 15 {
+			snake[0] = snakeHead - 15
+		} else {
+			snake[0] = snakeHead + 1
+		}
+		break
+	case UP:
+		if snakeHead < 16 {
+			snake[0] = snakeHead + 128 - 16
+		} else {
+			snake[0] = snakeHead - 16
+		}
+		break
+	case LEFT:
+		if snakeHead%16 == 0 {
+			snake[0] = snakeHead + 15
+		} else {
+			snake[0] = snakeHead - 1
+		}
+		break
+	case DOWN:
+		if snakeHead > 112 {
+			snake[0] = snakeHead % 16
+		} else {
+			snake[0] = snakeHead + 16
+		}
+		break
+	}
+
+	drawRect(snake[0])
+	if apple == snake[0] {
+		snakeLength++
+		apple += 7 * snakeLength
+		if apple > 127 {
+			apple -= 128
+		}
+
+		drawApple(apple)
+	} else {
+		for i = snakeLength - 1; i > 2; i-- {
+			if snake[i] == snake[0] {
+				startGame()
+				return
+			}
+		}
+		clearRect(snake[snakeLength-1])
+	}
+	for i = snakeLength - 1; i > 0; i-- {
+		snake[i] = snake[i-1]
+	}
+}
+
+func startGame() {
+	song()
+	display.FillScreen(0xFF)
+	display.FillScreen(0x00)
+	snakeLength = 4
+	snakeDirection = RIGHT
+
+	snake[0] = 34
+
+	apple = 100
+	drawApple(apple)
+}
+
+func drawRect(i uint8) {
+	display.SetPos(8*(i%16), 8*(i/16))
 	display.DataStart()
-	display.DataByte(0xC0)
-	display.DataByte(0x60)
-	display.DataByte(0x30)
-	display.DataByte(0x7C)
-	display.DataByte(0x30)
-	display.DataByte(0x60)
-	display.DataByte(0xC0)
+	display.DataByte(0xFF)
+	display.DataByte(0xFF)
+	display.DataByte(0xFF)
+	display.DataByte(0xFF)
+	display.DataByte(0xFF)
+	display.DataByte(0xFF)
+	display.DataByte(0xFF)
+	display.DataByte(0xFF)
 	display.DataStop()
 }
 
-func ClearPlayer(x uint8) {
-	display.SetPos(x, 63)
+func clearRect(i uint8) {
+	display.SetPos(8*(i%16), 8*(i/16))
 	display.DataStart()
+	display.DataByte(0x00)
 	display.DataByte(0x00)
 	display.DataByte(0x00)
 	display.DataByte(0x00)
@@ -114,23 +172,17 @@ func ClearPlayer(x uint8) {
 	display.DataStop()
 }
 
-func DrawShoot(index uint8) {
-	i := shootsY[index] % 8
-	if i >= 5 && shootsY[index] < 56 {
-		display.SetPos(shootsX[index], 8*(shootsY[index]/8)+8)
-		display.DataStart()
-		display.DataByte(0x0F >> (8 - i))
-		display.DataStop()
-	}
-	if i == 4 && shootsY[index] < 56 {
-		display.SetPos(shootsX[index], 8*(shootsY[index]/8)+8)
-		display.DataStart()
-		display.DataByte(0x00)
-		display.DataStop()
-	}
-	display.SetPos(shootsX[index], 8*(shootsY[index]/8))
+func drawApple(i uint8) {
+	display.SetPos(8*(i%16), 8*(i/16))
 	display.DataStart()
-	display.DataByte(0x0F << i)
+	display.DataByte(0xFF)
+	display.DataByte(0x81)
+	display.DataByte(0x81)
+	display.DataByte(0x81)
+	display.DataByte(0x81)
+	display.DataByte(0x81)
+	display.DataByte(0x81)
+	display.DataByte(0xFF)
 	display.DataStop()
 }
 
@@ -141,4 +193,12 @@ func beep(count, delay int) {
 		speaker.Low()
 		time.Sleep(time.Duration(delay) * time.Microsecond)
 	}
+}
+
+func song() {
+	beep(50, 250)
+	time.Sleep(100 * time.Millisecond)
+	beep(150, 150)
+	time.Sleep(100 * time.Millisecond)
+	beep(250, 50)
 }
